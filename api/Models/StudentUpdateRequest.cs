2ec2986b4076f208a48c796cc88ef0f6dd8f4217
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2024-07-08 22:29:58 -0300

    feat:[back] adjust endpoint create student

---
+++ api/Models/StudentUpdateRequest.cs
@@ -0,0 +1,12 @@
+
+namespace apidotnetreact.Models
+{
+    public class StudentUpdateRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+
+    }
+}
