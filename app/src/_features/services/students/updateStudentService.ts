79aac0bd28449c389d98556492b3f7de83f05590
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2024-08-15 18:00:02 -0300

    feat: refactor in file domain

--- app/src/_features/services/students/updateStudentService.ts
+++ app/src/_features/services/students/updateStudentService.ts
@@ -1,11 +1,11 @@
-import { StudentUpdateRequest } from "../../domain/student";
+import { StudentRequest } from "../../domain/student";
 import { api } from "../api/axios-config";
 
 export const update = async (
-  student: StudentUpdateRequest
+  student: StudentRequest
 ): Promise<number> => {
   const urlRelative = `/student/Update/${student.id}`;
-  const response = await api.put<StudentUpdateRequest>(urlRelative, {
+  const response = await api.put<StudentRequest>(urlRelative, {
     id: student.id,
     name: student.name,
     email: student.email,
